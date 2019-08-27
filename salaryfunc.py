67053b1a21caaa654387768f9eef91504265b4d5
nehashingote <54305741+nehashingote@users.noreply.github.com>
2019-08-27 13:49:14 +0530

    Add files via upload

---
+++ salaryfunc.py
@@ -0,0 +1,14 @@
+def salary(x):
+    ta=x*.1
+    da=x*.2
+    hra=x*.3
+    pf=x*.12
+    gross=x*1.6
+    net=x*1.48
+    print("basic salary=",x)
+    print("ta=",ta)
+    print("da=",da)
+    print("hra=",hra)
+    print("pf=",pf)
+    print("gross=",gross)
+    print("net salary=",net)
